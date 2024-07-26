86afaa7004d8436037689671c1b2599963fe61b3
ishad17 <ishadh99@gmail.com>
2024-07-26 07:11:58 +0530

    july 25th 2024

---
+++ CSS/file/photooo/scrpit.js
@@ -0,0 +1,36 @@
+
+function add7(a){
+    return a + 7 
+}
+
+console.log(add7(10));
+
+////////////////////////////////////////////////////////////////////
+
+
+function capitalize(textInput){
+
+    
+    if (textInput.length === 0){
+        return textInput.toUpper();
+    }
+
+
+    let mancunian = textInput;
+
+    return console.log(resultText)
+
+}
+
+capitalize(prompt('Input text here..'));
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+function addd(a,j){
+
+    return console.log(a + j + " is the answer")
+}
+
+addd(49,12)
